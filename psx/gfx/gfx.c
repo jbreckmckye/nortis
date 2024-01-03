ed2589ff29c32f217dc3b81770194b77d3538b9d
jbreckmckye <3148617+jbreckmckye@users.noreply.github.com>
2024-01-03 16:00:21 +0000

    ui code

--- psx/gfx/gfx.c
+++ psx/gfx/gfx.c
@@ -6,6 +6,16 @@
 
 static RenderContext ctx = {0};
 
+// Special characters in font:
+// $ = happy emoji, % = sad emoji, () = left/right arrow, & = up arrow, ' = down arrow
+static const char* TITLE = "PLAY NOTRIS $";
+static const char* GAMEOVER = "GAME OVER %";
+static const char* CONTROL_MOVE = "() MOVE, & ROTATE"; // () maps to l/r arrow, & maps to up arrow
+static const char* CONTROL_DROP = "' FAST DROP";
+static const char* SCORE = "SCORE";
+static const char* RESTART = "[START] TO RESTART";
+static const char* KREDITS = "KREDITS JBRECKMCKYE + PSNOOBSDK ^_^"
+
 extern const uint32_t tim_font[];
 extern const size_t tim_font_size;
 
