77816e3426194f4774672b54d77a6ad66c091e9c
Jimmy Breck-McKye <3148617+jbreckmckye@users.noreply.github.com>
2024-06-22 23:59:26 +0100

    Generate random numbers

--- psx/main.c
+++ psx/main.c
@@ -7,11 +7,20 @@
 #include <psxgpu.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <psxapi.h>
 
-#include "./gfx/gfx.h"
-#include "./gfx/ui.h"
-#include "./io/pad.h"
+#include "gfx/gfx.h"
+#include "gfx/ui.h"
+#include "io/pad.h"
 #include "defs.h"
+
+int random() {
+  // Pseudo-random number based on function timing (first piece is deterministic)
+  int rootCount = GetRCnt(0);
+  srand(rootCount);
+  int result = rand();
+}
 
 int main(int argc, char** argv) {
   gfx_init();
@@ -22,10 +31,13 @@ int main(int argc, char** argv) {
 
   while (1) {
     pad_debug();
+    if (pad_buttons1()) {
+      printf("RANDOM NUMBER %d \n", random());
+    }
 
     ui_renderPlayArea();
     ui_renderTitle(true);
-    ui_renderScores(1234, 4321);
+    ui_renderScores(5678, 9012);
     ui_renderControls(true);
     ui_renderKredits();
 
